bac50c0f6f880d629dbd852983766336bc01f9f6
skeyan <eyan0749@gmail.com>
2021-10-21 18:47:18 -0400

    Get debit and credit data from api endpoint in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from "axios";
+
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
@@ -10,11 +12,13 @@ class App extends Component {
     super();
 
     this.state = {
-      accountBalance: 14568.27,
+      accountBalance: 0,
       currentUser: {
         userName: 'my_username',
         memberSince: '01/22/1999',
-      }
+      },
+      debits: [],
+      credits: []
     }
   }
 
@@ -22,6 +26,27 @@ class App extends Component {
     const newUser = {...this.state.currentUser}
     newUser.userName = logInInfo.userName
     this.setState({currentUser: newUser})
+  }
+
+  async componentDidMount() {
+    let debits = await axios.get("https://moj-api.herokuapp.com/debits");
+    let credits = await axios.get("https://moj-api.herokuapp.com/credits");
+
+    debits = debits.data
+    credits = credits.data
+
+    let debitSum = 0, creditSum = 0;
+    debits.forEach((debit) => {
+      debitSum += debit.amount;
+    })
+    credits.forEach((credit) => {
+      creditSum += credit.amount;
+    })
+
+    let accountBalance = (creditSum - debitSum).toFixed(2);
+    this.setState({
+      debits, credits, accountBalance
+    });
   }
 
   render() {
